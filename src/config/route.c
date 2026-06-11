0d0a3274d663525a1afb1cff6e7630289af5f30e
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-06-11 20:23:34 +0300

    change to external

--- src/config/route.c
+++ src/config/route.c
@@ -1,13 +1,14 @@
 #include "route.h"
 
+static struct Route __routes[] = {
+    {.name = "index", .path = "/"},
+};
+
 int get_routes(struct Route **routes, size_t *count)
 {
-    static struct Route temp[] = {
-        {.name = "index", .path = "/"},
-    };
-    *routes = temp;
+    *routes = __routes;
 
-    size_t count_temp = sizeof(temp) / sizeof(temp[0]);
+    size_t count_temp = sizeof(__routes) / sizeof(__routes[0]);
     *count = count_temp;
 
     return 0;
