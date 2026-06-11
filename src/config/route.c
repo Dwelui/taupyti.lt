bd856a1e161bea94ae8ed759da01c6859f2cc40e
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-06-11 20:21:55 +0300

    fix get_routes

--- src/config/route.c
+++ src/config/route.c
@@ -1,6 +1,14 @@
 #include "route.h"
 
-int get_routes(struct Route *routes, size_t *count)
+int get_routes(struct Route **routes, size_t *count)
 {
+    static struct Route temp[] = {
+        {.name = "index", .path = "/"},
+    };
+    *routes = temp;
+
+    size_t count_temp = sizeof(temp) / sizeof(temp[0]);
+    *count = count_temp;
+
     return 0;
 }
--- src/config/route.h
+++ src/config/route.h
@@ -2,12 +2,13 @@
 #define ROUTE_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 struct Route {
     char name[1024];
     char path[1024];
 };
 
-int get_routes(struct Route *routes, size_t *count);
+int get_routes(struct Route **routes, size_t *count);
 
 #endif // ROUTE_H
