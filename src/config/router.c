7920b31740f3b1a48d1657eaa57d4bc75460923e
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-06-15 21:55:10 +0300

    handle no route match

--- src/config/router.c
+++ src/config/router.c
@@ -5,6 +5,8 @@ int route(const HttpRequest *req, HttpResponse *res)
 {
     if (strcmp(req->path, "/") == 0) {
         return default_controller_index(req, res);
+    } else {
+        return -1;
     }
 
     return 0;
