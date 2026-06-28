6018d66fc4636c00825c35275ead89ded5a52281
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-06-28 16:29:09 +0300

    refactor string tests

--- dep/string/tests/main.c
+++ dep/string/tests/main.c
@@ -1,5 +1,47 @@
 #include "../../test/src/test.h"
-#include "unit/test_string.h"
+#include "../src/string.h"
+
+#include <string.h>
+
+void test_string_from_cstring(TestCaseOutput *output)
+{
+    string_from_cstring("Hello World!");
+}
+
+void test_string_from_cstring_correct_count(TestCaseOutput *output)
+{
+    char *original = "Hello World!";
+
+    string string = string_from_cstring(original);
+
+    test_int_is_equal(output, 12, string.count);
+}
+
+void test_string_from_cstring_data_is_same_as_original(TestCaseOutput *output)
+{
+    char *original = "Hello World!";
+
+    string string = string_from_cstring(original);
+
+    for (size_t i = 0; i < strlen(original); i++) {
+        if (original[i] != string.data[i]) {
+            test_fail(output, "letters do not match");
+        }
+    }
+}
+
+void test_string_to_cstring(TestCaseOutput *output)
+{
+    string original = string_from_cstring("Hello World!");
+
+    char *cstring = string_to_cstring(original);
+
+    for (size_t i = 0; i < strlen(cstring); i++) {
+        if (original.data[i] != cstring[i]) {
+            test_fail(output, "letters do not match");
+        }
+    }
+}
 
 TestCase unit_tests[] = {
     TEST_CASE_REGISTER(test_string_from_cstring),
@@ -7,6 +49,7 @@ TestCase unit_tests[] = {
     TEST_CASE_REGISTER(test_string_from_cstring_data_is_same_as_original),
     TEST_CASE_REGISTER(test_string_to_cstring),
 };
+
 
 int main()
 {
