8c8ea152acd103e6c79e3eb86d8652bfb373d621
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-07-26 18:50:12 +0300

    fix test assertion

--- dep/sss/tests/unit/http/test_response.c
+++ dep/sss/tests/unit/http/test_response.c
@@ -11,7 +11,7 @@ void test_response_simple_ok_is_valid(TestCaseOutput *output)
     string_array lines = string_split(response->raw, "\r\n");
     string statusLine = lines.items[0];
 
-    test_cstring_is_equal_to_string(output, "HTTP/1.1 200 OK\r\n\r\n", statusLine);
+    test_cstring_is_equal_to_string(output, "HTTP/1.1 200 OK", statusLine);
 
     response_free(response);
     string_array_free(lines);
