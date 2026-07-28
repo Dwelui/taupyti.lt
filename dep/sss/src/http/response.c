6e3d3ba756d206372bf2aa12a53abcf683eaa84f
Mantas Motuzas <mantas.motuzas41@gmail.com>
2026-07-28 19:38:23 +0300

    fix up

--- dep/sss/src/http/response.c
+++ dep/sss/src/http/response.c
@@ -23,15 +23,15 @@ string response_to_string(const Response *res)
     string_array status_line = string_array_create(4);
 
     string protocol_version = http_version_to_string(res->protocol);
-    string_array_push(status_line, protocol_version);
+    string_array_push(&status_line, protocol_version);
 
     string status_code = string_from_int(res->status_code);
-    string_array_push(status_line, status_code);
+    string_array_push(&status_line, status_code);
 
     string reason_phrase = response_reason_phrase_to_string(res->reason_phrase);
-    string_array_push(status_line, reason_phrase);
+    string_array_push(&status_line, reason_phrase);
 
-    string_array_push(status_line, string_empty()); // For additional \r\n.
+    string_array_push(&status_line, string_empty()); // For additional \r\n.
     string result = string_join(status_line, "\r\n");
 
     string_array_free(status_line);
